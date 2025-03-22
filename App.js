f260ff4a42da73b6d2b04c7d28bc88e19bed0a1f
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-22 22:39:27 +0000

    Getting Started with the App & Outputting Meal data

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Categories from './screens/Categories';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Categories/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ 
   },
 });
---
+++ screens/Categories.js
@@ -0,0 +1,21 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummydata";
+
+import React from "react";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem({ item }) {
+  return <CategoryGridTile title={item.title} color={item.color} />;
+}
+
+function Categories() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+}
+
+export default Categories;
