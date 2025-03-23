8f9d0baba222ed4634ad14a01a5fa730f5f56ee3
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-23 15:32:31 +0000

    Displaying Meals

---
+++ components/MealItem.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MealItem = ({title}) => {
+  return (
+    <View>
+      <Text>
+        {title}
+      </Text>
+    </View>
+  )
+}
+
+export default MealItem
+
+const styles = StyleSheet.create({})
--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import { MEALS } from "../data/dummydata";
+import MealItem from "../components/MealItem";
 
-const MealsOverview = ({route}) => {
-  const catId=route.params.categoryId
+const MealsOverview = ({ route }) => {
+  const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem({item}){
+    return <MealItem title={item.title}/>
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>MealsOverview- {catId}</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
     </View>
   );
 };
