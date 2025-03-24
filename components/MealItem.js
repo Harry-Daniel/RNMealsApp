5361cd75aa0454a36efeb3234aa417ccb84e74f1
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-24 22:54:36 +0000

    Finishing the Meal Detail Screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 const MealItem = ({ id,title, imageUrl, duration, complexity, affordability }) => {
   const navigation = useNavigation()
@@ -30,11 +31,7 @@ const MealItem = ({ id,title, imageUrl, duration, complexity, affordability }) =
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}mins</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+         <MealDetails duration={duration} complexity={complexity} affordability={affordability}/>
         </View>
       </Pressable>
     </View>
@@ -69,16 +66,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     margin: 8,
   },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
-  },
+ 
   buttonPressed: {
     opacity: 0.5,
   },
--- screens/MealDetail.js
+++ screens/MealDetail.js
@@ -1,15 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+import { MEALS } from "../data/dummydata";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
+
 const MealDetail = ({ route }) => {
-  const catId = route.params.categoryId;
+  const mealId = route.params.categoryId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
-    <View>
-      <Text>MealsDetail-{catId}</Text>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        complexity={selectedMeal.complexity}
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+
+      <View style={styles.listContainer}>
+        <Subtitle>Ingredients</Subtitle>
+        <List data={selectedMeal.ingredients} />
+        <Subtitle>Steps</Subtitle>
+        <List data={selectedMeal.steps} />
+      </View>
+    </ScrollView>
   );
 };
 
 export default MealDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer:{
+    marginBottom:32
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listContainer:{
+    width:'80%',
+    margin:'auto'
+  },
+});
