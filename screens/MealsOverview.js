c7fa8c40474f7c131e2f94923e3320b3345f1058
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-24 14:56:51 +0000

    Setting Navigation Option Dynamically

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,24 +1,43 @@
 import { StyleSheet, FlatList, View } from "react-native";
-import { MEALS } from "../data/dummydata";
+import { MEALS, CATEGORIES } from "../data/dummydata";
 import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
 
-const MealsOverview = ({ route }) => {
+const MealsOverview = ({ route, navigation }) => {
   const catId = route.params.categoryId;
+
   const displayedMeals = MEALS.filter((meal) => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
 
-  function renderMealItem({item}){
-    return <MealItem title={item.title} imageUrl={item.imageUrl}
-    affordability={item.affordability} complexity={item.complexity}
-    duration={item.duration}
-    />
-  }
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
+  function renderMealItem({ item }) {
+    return (
+      <MealItem
+        title={item.title}
+        imageUrl={item.imageUrl}
+        affordability={item.affordability}
+        complexity={item.complexity}
+        duration={item.duration}
+      />
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
